54e2284a6b243979899e3c27da663405c70607a8
sandyud <liuyushan1223@gmail.com>
2018-01-08 19:32:43 -0500

    Fix the issue that can create a project but cannot commit in Github

---
+++ test.py
@@ -0,0 +1,9 @@
+"""
+Test if the PyCharm has connected to my github account
+
+1. First error: user.email and user.name is not consistent with Github.
+    Fix it in this way:
+        Run git config --global user.email "you@example.com"
+        git config --global user.name "Your Name" to set your account's default identity.
+        Omit --global to set the identity only in this repository.
+"""
